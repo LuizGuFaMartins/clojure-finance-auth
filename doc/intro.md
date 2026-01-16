6270f5e7e6f7ca95d33e9fea24de4226d3f9ae5d
luiz.martins <luizgufamartins@gmail.com>
2026-01-16 16:21:31 -0300

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-finance-auth/clojure_finance_auth
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_finance_auth/clojure_finance_auth_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-finance-auth.clojure-finance-auth-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [clojure-finance-auth.clojure-finance-auth :as sut])) ; system under test
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
